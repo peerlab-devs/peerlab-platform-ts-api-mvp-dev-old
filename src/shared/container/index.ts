d50221db50c184c9f1ae9dffadc4778188af69b4
amaralc <amaral.calil@gmail.com>
2020-12-28 14:10:33 -0300

    FEATURE / Registra esquema de usuario em container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,8 +3,16 @@ import { container } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import ICreateUserSchema from '@modules/users/providers/ValidationProvider/models/ICreateUserSchema';
+import YupCreateUserSchema from '@modules/users/providers/ValidationProvider/implementations/YupCreateUserSchema';
+
 /** Registra mesma instância de repositório quando algum serviço chamar o id registrado entre aspas */
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<ICreateUserSchema>(
+  'CreateUserSchema',
+  YupCreateUserSchema,
 );
