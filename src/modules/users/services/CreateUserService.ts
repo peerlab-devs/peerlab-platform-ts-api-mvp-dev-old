8c1de322957fd037ee9e066ecf129b0fd9e344ad
amaralc <amaral.calil@gmail.com>
2020-12-28 14:11:30 -0300

    FEATURE / Injeta dependencia e aplica validacao e regras de negocio

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -5,18 +5,37 @@ import AppError from '@shared/errors/AppError';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import ICreateUserSchema from '@modules/users/providers/ValidationProvider/models/ICreateUserSchema';
 
 @injectable()
 class CreateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CreateUserSchema')
+    private createUserSchema: ICreateUserSchema,
   ) {}
 
-  public async execute({ name, email }: ICreateUserDTO): Promise<User> {
-    // TODO
-    const user = this.usersRepository.create({ email, name });
+  public async execute(data: ICreateUserDTO): Promise<User> {
+    /** Avalia formato dos dados */
+    const validData = await this.createUserSchema.isValid(data);
 
+    if (!validData) {
+      throw new AppError('Dados informados não são válidos.');
+    }
+
+    /** Aplica regras de negócio */
+    const userExists = await this.usersRepository.findByEmail(data.email);
+
+    if (userExists) {
+      throw new AppError('Email já está sendo utilizado por outro usuário');
+    }
+
+    /** Executa service */
+    const user = await this.usersRepository.create(data);
+
+    /** Retorna resultado */
     return user;
   }
 }
